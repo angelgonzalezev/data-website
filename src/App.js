0bf6963fe190d058d525afb60ce35f5031bee8a4
Ángel González <aangel.gg@hotmail.com>
2023-01-04 18:08:20 +0100

    Created Hero section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Hero from "./containers/Hero";
 import Navbar from "./containers/Navbar";
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 }
